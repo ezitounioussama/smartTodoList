b0246f66d887277bbf440daa91fa3ab3151e9322
ezitounioussama <zitounioussama95@gmail.com>
2023-02-01 00:45:56 +0100

    I guess I understand components now 00:45

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
 import "./style.css";
 </script>
 
 <template>
-  <div class="relative">
+  <div class="relative my-8 mx-5">
     <div
       class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
     >
@@ -24,18 +22,38 @@ import "./style.css";
         />
       </svg>
     </div>
+
     <input
-      type="search"
-      id="default-search"
-      class="block w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      v-model="message"
+      type="text"
+      class="block w-full p-4 pl-12 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       placeholder="Add Tasks..."
-      required
     />
+
     <button
-      type="submit"
+      @click="onSubmit"
+      type="button"
       class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
     >
       Add
     </button>
   </div>
+  <article class="rounded-xl border border-gray-700 bg-gray-800 p-4 mx-5">
+    <List />
+  </article>
 </template>
+
+<script>
+import List from "./components/list.vue";
+export default {
+  data() {
+    return {
+      message: "",
+      onSubmit() {
+        console.log(this.message);
+      },
+      components: List,
+    };
+  },
+};
+</script>
