c215472603c199e0bab191ae26eb15507233f2fe
ezitounioussama <zitounioussama95@gmail.com>
2023-01-31 15:44:43 +0100

    first test

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import "./style.css";
+</script>
+
+<template>
+  <div class="relative">
+    <div
+      class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 text-blue-600"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M15 15l6-6m0 0l-6-6m6 6H9a6 6 0 000 12h3"
+        />
+      </svg>
+    </div>
+    <input
+      type="search"
+      id="default-search"
+      class="block w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      placeholder="Add Tasks..."
+      required
+    />
+    <button
+      type="submit"
+      class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+    >
+      Add
+    </button>
+  </div>
+</template>
